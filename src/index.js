17311fb3f8d58a4436f6c2394fe84b56dd37836f
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-12-13 11:02:47 +0100

    Add exploratory testing for indentation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+function indentationTesting(number) {
+  const result = number * 2
+    return result
+}
+
+function indentationTestingWithManyParameters(
+  anUnnecessarilyLongParameterNameForTheHeckOfItNumber, 
+  someOtherParameterThatShouldBeAString) {
+  result = anUnnecessarilyLongParameterNameForTheHeckOfItNumber * 2
+  return someOtherParameterThatShouldBeAString + result
+}
+
 function arrowFunctionTest(arrFunc) {
   let result = arrFunc(2)
   console.log(result)
