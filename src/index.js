f6b221e1cd7e27d29176191cea677bc92f57c697
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-12-13 11:22:21 +0100

    Add exploratory testing for semi

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ function indentationTestingWithManyParameters(
   return someOtherParameterThatShouldBeAString + result
 }
 
+function noExtraSemiTest() {
+  console.log('I really should not end with semi-colons...');
+}
+
 function arrowFunctionTest(arrFunc) {
   let result = arrFunc(2)
   console.log(result)
 }
 
 arrowFunctionTest((a) => { return a + 1})
+arrowFunctionTest(a => { return a * 2})
