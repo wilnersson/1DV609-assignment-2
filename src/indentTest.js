f4593dd760893612928999a00a255a438faa9e97
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-12-14 13:57:23 +0100

    Refactor indent testing

---
+++ src/indentTest.js
@@ -0,0 +1,11 @@
+function indentationTesting(number) {
+  const result = number * 2
+    return result
+}
+
+function indentationTestingWithManyParameters(
+  anUnnecessarilyLongParameterNameForTheHeckOfItNumber, 
+  someOtherParameterThatShouldBeAString) {
+  result = anUnnecessarilyLongParameterNameForTheHeckOfItNumber * 2
+  return someOtherParameterThatShouldBeAString + result
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-function indentationTesting(number) {
-  const result = number * 2
-    return result
-}
-
-function indentationTestingWithManyParameters(
-  anUnnecessarilyLongParameterNameForTheHeckOfItNumber, 
-  someOtherParameterThatShouldBeAString) {
-  result = anUnnecessarilyLongParameterNameForTheHeckOfItNumber * 2
-  return someOtherParameterThatShouldBeAString + result
-}
-
 function noExtraSemiTest() {
   console.log('I really should not end with semi-colons...');
 }
