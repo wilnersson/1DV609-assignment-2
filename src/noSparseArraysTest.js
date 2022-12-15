e3d859f50bcdfae5d3b5c52376bacfd589b68bdc
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-12-15 09:50:03 +0100

    Add no-sparse-arrays testing

---
+++ src/noSparseArraysTest.js
@@ -0,0 +1,11 @@
+const testArrayOne = ['hello', 'world']
+
+const testArrayTwo = ['hello', , 'world']
+
+let testArrayThree = [1,2,3]
+
+let testArrayFour = [1,,3,4]
+
+const testArrayFive = [,1,'hello']
+
+const testArraySix = ['hello',1]
