5434c954e5ec709d3ed5c9ef6f802c1a9905b829
Maria Olsson <mo223id@student.lnu.se>
2022-12-20 18:44:24 +0100

    samall changes

--- src/noConstAssignTest.js
+++ src/noConstAssignTest.js
@@ -8,8 +8,13 @@ const aVariable = 0
 // reassigning
 aVariable = 10
 
-
 aVariable += 1
 
-const c = 0
-++c;
+// assigning av value to a const variable
+const c = ''
+
+c = 10
+
+let c = c
+
+c = d
--- src/noNewTest.js
+++ src/noNewTest.js
@@ -1,8 +1,9 @@
-// This rule is aimed at maintaining consistency and convention by disallowing constructor calls using 
-// the new keyword that do not assign the resulting object to a variable.
+/**
+ * This rule is aimed at maintaining consistency and convention by disallowing constructor calls using 
+ * the new keyword that do not assign the resulting object to a variable.
+ */
 
 // new is used correct
-
 a === new ObjectInstance()
 
 // here without new, and is then a function call
